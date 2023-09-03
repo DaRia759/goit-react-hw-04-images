da8eb959be7f9baaddc525eae017ddfa1bd0b801
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-09-03 16:53:52 +0200

    Refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Component, useState } from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 import css from './App.module.css';
 import SearchBar from './SearchBar/Searchbar';
 import { ToastContainer, toast } from "react-toastify";
@@ -9,7 +9,7 @@ import Modal from './Modal/Modal';
 import Button from "./Button/Button";
 import Spinner from "./Loader/Loader";
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from "react";
+
 
 
 const PER_PAGE = 12;
@@ -20,12 +20,12 @@ export const App = () => {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [showLoader, setShowLoader] = useState(false);
-  const [error,setError] = useState(null);
+  const [error, setError] = useState(null);
   const [modalURL, setModalURL] = useState('');
   
 
   useEffect(() => {
-    const handleFormOnSubmit = searchWord => {
+    const handleFormOnSubmit = (searchWord) => {
       setSearchWord(searchWord);
       setImages([]);
       setPage(1);
@@ -34,157 +34,93 @@ export const App = () => {
       setError(null);
       setModalURL('');
     };
-  }), [searchWord, images, page, totalPages, showLoader, error, modalURL];
 
-  const onImageClick = url => {
-    modalURL:url;
+    if (searchWord) {
+      const fetchData = async () => {
+        setShowLoader(true);
+        try {
+          const result = await getImage.fetchImagesBundle({
+            query: searchWord,
+            page,
+            perPage: PER_PAGE,
+          });
+          
+          if (result.totalHits === 0) {
+            toast.warning('Sorry! There is no result for your request');
+            setImages([]);
+            setShowLoader(false);
+            return;
+          };
+
+          if (page === 1) {
+            toast.info(`There are ${result.totalHits} images we found`);
+            setTotalPages(Math.ceil(result.totalHits / PER_PAGE));
+          }
+
+          const hits = result.hits.map((element) => ({
+            id: element.id,
+            webformatURL: element.webformatURL,
+            largeImageURL: element.largeImageURL,
+            user: element.user,
+          }));
+
+          setImages((prevImages) => [...prevImages, ...hits]);
+        } catch (error) {
+          setError(error.message);
+          toast.error(`Error occured ${error.message}`)
+        } finally {
+          setShowLoader(false)
+        }
+      };
+
+      fetchData();
+    }
+  }, [searchWord, page]);
+
+   
+
+  const onImageClick = (url) => {
+    setModalURL(url);
   };
 
   const cleanURL = () => {
-    modalURL:'';
+    setModalURL('');
   };
 
   const handleLoadMore = () => {
-        if (page < totalPages) {
-            this.setState(prevState => ({ page: prevState.page + 1 }));
+        if (page +1 <= totalPages) {
+            setPage((prevState) => prevState.page + 1);
         }
   };
 
+
+  const isLoadMoreDisabled = !images.length && searchWord === '';
+
   return (
-            <>
-                <div className={css.app}>
-                    <SearchBar onSubmit={handleFormOnSubmit} />
-                    <ImageGallery images={images} onClick={onImageClick} />
-                    <Spinner show={showLoader} />
-                    {!!images.length && (
-                    <Button onClick={handleLoadMore} disabled={isLoadMoreDisabled} style={{ display: isLoadMoreDisabled ? 'none' : 'block' }} />
-                    )}
-                    {Boolean(modalURL) && <Modal url={modalURL} cleanURL={cleanURL} />}
-                </div>
-                <ToastContainer
-                    position="bottom-center"
-                    autoClose={3000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
-            </>
-        )
+    <>
+      <div className={css.app}>
+        <SearchBar onSubmit={handleFormOnSubmit} />
+        <ImageGallery images={images} onClick={onImageClick} />
+        <Spinner show={showLoader} />
+            {!!images.length && (
+        <Button onClick={handleLoadMore} disabled={isLoadMoreDisabled} style={{ display: isLoadMoreDisabled ? 'none' : 'block' }} />
+            )}
+        {Boolean(modalURL) && <Modal url={modalURL} cleanURL={cleanURL} />}
+      </div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      </>
+  )
 };
 
-
-// export default class App extends Component {
-//     state = {
-//         searchWord: '',
-//         images: [],
-//         page: 1,
-//         totalPages: 1,
-//         showLoader: false,
-//         error: null,
-//         modalURL: '',
-//     };
-
-//     async componentDidUpdate(prevProps, prevState) {
-//         const searchedWordUpdate =
-//             prevState.searchWord !== this.state.searchWord;
-//         const pageUpdate = prevState.page !== this.state.page;
-
-//         if (searchedWordUpdate || pageUpdate) {
-//             this.setState({ showLoader: true });
-//             try {
-//                 const result = await getImage.fetchImagesBundle({
-//                     query: this.state.searchWord,
-//                     page: this.state.page,
-//                     perPage: PER_PAGE,
-//                 });
-
-//                 if (result.totalHits === 0) {
-//                     toast.warning('Sorry! There is no result for your request'
-//                     );
-//                     this.setState({
-//                         images: [],
-//                         showLoader: false
-//                     });
-//                     return;
-//                 }
-//                 if (searchedWordUpdate) {
-//                     toast.info(`There are ${result.totalHits} images we found`);
-//                     this.setState({ totalPages: Math.ceil(result.totalHits / PER_PAGE) });
-//                 }
-//                 const hits = result.hits.map(element => {
-//                     return {
-//                         id: element.id,
-//                         webformatURL: element.webformatURL,
-//                         largeImageURL: element.largeImageURL,
-//                         user: element.user,
-//                     };
-//                 });
-//                 const newState = [...this.state.images, ...hits];
-//                 this.setState({
-//                     images: newState,
-//                     showLoader: false
-//                 });
-
-//             } catch (error) {
-//                 this.setState({ error: error.message });
-//                 toast.error(`Error occured ${this.state.error}`);
-//             } finally {
-//                 this.setState({ showLoader: false });
-//             }
-//         }
-//     }
-
-//     handleFormOnSubmit = searchWord => {
-//         this.setState({ searchWord, page: 1, images: []});
-//     };
-
-//     onImageClick = url => {
-//         this.setState({ modalURL:url });
-//     };
-
-//     cleanURL = () => {
-//         this.setState({ modalURL:'' });
-//     };
-
-//     handleLoadMore = () => {
-//         if (this.state.page < this.state.totalPages) {
-//             this.setState(prevState => ({ page: prevState.page + 1 }));
-//         }
-//     };
-
-
-//     render() {
-//         const isLoadMoreDisabled = !this.state.images.length && this.state.searchWord === '';
-
-//         return (
-//             <>
-//                 <div className={css.app}>
-//                     <SearchBar onSubmit={this.handleFormOnSubmit} />
-//                     <ImageGallery images={this.state.images} onClick={this.onImageClick} />
-//                     <Spinner show={this.state.showLoader} />
-//                     {!!this.state.images.length && (
-//                     <Button onClick={this.handleLoadMore} disabled={isLoadMoreDisabled} style={{ display: isLoadMoreDisabled ? 'none' : 'block' }} />
-//                     )}
-//                     {Boolean(this.state.modalURL) && <Modal url={this.state.modalURL} cleanURL={this.cleanURL} />}
-//                 </div>
-//                 <ToastContainer
-//                     position="bottom-center"
-//                     autoClose={3000}
-//                     hideProgressBar={false}
-//                     newestOnTop={false}
-//                     closeOnClick
-//                     rtl={false}
-//                     pauseOnFocusLoss
-//                     draggable
-//                     pauseOnHover
-//                     theme="light"
-//                 />
-//             </>
-//         )
-//     }
-// };
+export default App;
--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -1,53 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {toast} from 'react-toastify';
 import css from './SearchBar.module.css';
 
-export default class SearchBar extends React.Component { 
-    state = {
-        searchWord: '',
-    }
+const SearchBar = ({ onSubmit }) => {
+    const [searchWord, setSearchWord] = useState('');
 
-    handleChange = e => {
-        this.setState({
-            searchWord: e.currentTarget.value.toLowerCase()
-        })
+    const handleChange = (e) => {
+        setSearchWord(e.currentTarget.value.toLowerCase());
     };
 
-    handleSubmit = event => {
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        if (this.state.searchWord.trim() === '') {
+        if (searchWord.trim() === '') {
             toast.error('Please enter your request');
             return;
         } 
-        this.props.onSubmit(this.state.searchWord.trim());
+        onSubmit(searchWord.trim());
     };
 
-    render() {
-        const { searchWord } = this.state;
-        return (
-            <header className={css.searchbar}>
-                <form className={css.searchForm} onSubmit={this.handleSubmit}>
-                    <button type="submit" className={css.button}>
-                        <span className={css.buttonLabel}>Search</span>
-                    </button>
+    return (
+        <header className={css.searchbar}>
+            <form className={css.searchForm} onSubmit={handleSubmit}>
+                <button type="submit" className={css.button}>
+                    <span className={css.buttonLabel}>Search</span>
+                </button>
 
-                    <input
-                        className={`${css.input} ${searchWord !== '' ? css.inputError : ''}`}
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        value={searchWord}
-                        onChange={this.handleChange}
-                        placeholder="Search images and photos"
-                    />
-                </form>
-            </header>   
-        )
-    }
-}
+                <input
+                    className={`${css.input} ${searchWord !== '' ? css.inputError : ''}`}
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    value={searchWord}
+                    onChange={handleChange}
+                    placeholder="Search images and photos"
+                />
+            </form>
+        </header>
+    );
+};
 
 SearchBar.propTypes = {
     onSubmit: PropTypes.func.isRequired,
 };
+
+export default SearchBar;
